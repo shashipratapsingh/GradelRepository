a3cae16d40169139c2eda5e5a1af5cc17894b2cd
Shashipratapsingh <itpro8577@gmail.com>
2024-01-03 14:34:00 +0530

    first commit

---
+++ src/main/java/com/SpringGradel/SpringGradelApplication.java
@@ -0,0 +1,14 @@
+package com.SpringGradel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradelApplication.class, args);
+		System.out.println("helow Team");
+	}
+
+}
